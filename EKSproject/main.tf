2bf42adbed8926b8ce28920e1d9dab0d9d109dae
deepika <deepika.dev.kale9@gmail.com>
2025-06-07 21:13:13 +0530

    files completed

---
+++ EKSproject/main.tf
@@ -0,0 +1,176 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "eks-project-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+resource "aws_subnet" "eks_subnet" {
+  count = 2
+  vpc_id                     = aws_vpc.eks-project-vpc.id
+  cidr_block                 = cidrsubnet(aws_vpc.eks-project-vpc.cidr_block, 8, count.index)
+  availability_zone          = element(["us-east-1a","us-east-1b"], count.index)
+  map_public_ip_on_launch    = true
+
+  tags = {
+    Name = "eks-subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks-project-vpc.id
+
+  tags = {
+    Name = "eks-internetgateway"
+  }
+}
+
+
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks-project-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "eks-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+count        = 2
+subnet_id    = aws_subnet.eks_subnet[count.index].id 
+route_table_id = aws_route_table.eks_route_table.id
+}
+
+resource "aws_security_group" "eks-cluster-sg" {
+  vpc_id = aws_vpc.eks-project-vpc.id
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster-sg"
+  }
+}
+
+resource "aws_security_group" "eks-node-sg" {
+  vpc_id = aws_vpc.eks-project-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-nodes-sg"
+ }
+}
+
+resource "aws_eks_cluster" "eks" {
+  name = "eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnet[*].id
+    security_group_ids = [aws_security_group.eks-cluster-sg.id]
+  }
+}
+
+resource "aws_eks_node_group" "eks" {
+  cluster_name     =   aws_eks_cluster.eks.name
+  node_group_name  =   "eks-node-group"
+  node_role_arn    =  aws_iam_role.eks_node_role.arn 
+  subnet_ids       =  aws_subnet.eks_subnet[*].id
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
+   } 
+
+  instance_types = ["t2.large"]
+
+  remote_access {
+    ec2_ssh_key = var.ssh_key_name
+    source_security_group_ids = [aws_security_group.eks-node-sg.id]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Statement" : [
+    {
+      "Effect" : "Allow",
+      "Principal" : {
+        "Service" : eks.amazonaws.com"
+      },
+      "Action" : "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Statement" : [
+    {
+      "Effect" : "Allow",
+      "Principal" : {
+        "Service" : eks.amazonaws.com"
+      },
+      "Action" : "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_role_policy" {
+  role = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_cni_policy" {
+  role = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_registry_policy" {
+  role = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}

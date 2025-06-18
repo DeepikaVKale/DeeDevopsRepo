1c3d2796c76d1ba4036692d947fcf26d39337ba3
deepika <deepika.dev.kale9@gmail.com>
2025-06-18 21:36:44 +0530

    tf for statefiles added

---
+++ backend/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "demo-tf-eks-state-bucket"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "tf-eksstate-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+}
+

e322750acec6ded2ca4c6d74633b849750c153d6
deepika <deepika.v.kale9@gmail.com>
2025-08-24 14:29:35 +0530

    outputs syntax updated

--- Project-2/outputs.tf
+++ Project-2/outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
   description = "VPC ID"
-  value       = aws_vpc.main.id
+  value       = module.vpc.vpc_id
 }
 
 output "subnet_1_ids" {
   description = "List of Subnet-1 IDs"
-  value       = aws_subnet.subnet_1[*].id
+  value       = module.vpc.subnet_1_ids
 }
 
 output "subnet_2_ids" {
   description = "List of Subnet-2 IDs"
-  value       = aws_subnet.subnet_2[*].id
+  value       = module.vpc.subnet_2_ids
 }
 
 output "PublicIP" {
     description = "Public IP of the EC2 instance"
-    value       = aws_instance.ec2.public_ip
+    value       = module.ec2.public_ip
 }

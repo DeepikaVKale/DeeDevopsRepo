4b3a0d258d35b018f00c325e8c2132cd4dc2ea50
deepika <deepika.v.kale9@gmail.com>
2025-08-25 18:01:41 +0530

    dock script

--- docker-script.sh
+++ docker-script.sh
@@ -1,4 +1,9 @@
-sudo apt update
+#!/bin.bash
+# Install Docker on Ubuntu
+
+sudo apt update -y
 sudo apt install -y docker.io
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo systemctl enable docker -y
+sudo systemctl start docker -y
+sudo usermod -aG docker ubuntu
+echo "Docker installed successfully" 

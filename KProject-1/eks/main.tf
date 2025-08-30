6043f8328199acd8be45f24154883e537bc82bf9
deepika <deepika.v.kale9@gmail.com>
2025-08-30 18:20:17 +0530

    eks

--- KProject-1/eks/main.tf
+++ KProject-1/eks/main.tf
@@ -136,7 +136,7 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy" {
-  role       = aws_iam_role.Eks_cluster_role.name
+  role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
